3aeebd61c31d3845d0569757899afbe8bb423f63
WhiteP42 <pau.rodriguez.riera@gmail.com>
2025-12-07 11:59:57 +0100

    new files

--- src/bioMEMEPy/extract.py
+++ src/bioMEMEPy/extract.py
@@ -2,8 +2,9 @@ from itertools import islice
 
 # Load sequences from the FASTA file
 
-def main(file):
+def extract(file):
     seq = []
     with open(file, 'r') as f:
         for line in islice(f, 0, None, 2):
             seq.append(line.rstrip('\n'))
+    return seq
