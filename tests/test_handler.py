dd3152d04746d472860694fcd45c469eefcacf59
WhiteP42 <pau.rodriguez.riera@gmail.com>
2025-12-15 14:56:36 +0100

    New test for snip()

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,4 +1,5 @@
 from bioMEMEPy.handler import PWM
+from bioMEMEPy.handler import snip
 from bioMEMEPy import monomers as mnm
 
 def test_pwm():
@@ -13,4 +14,9 @@ def test_pwm():
         total = 0
         for nucl in mnm.dna:
             total += pwm.matrix[nucl][i]
-        assert total == 1
+        assert total == 1 # Handle the float error!!
+
+def snip_test():
+    seq = 'ATACGTTAT'
+    snippet = snip(seq, 4, 4)
+    assert snippet == 'GTTA'
