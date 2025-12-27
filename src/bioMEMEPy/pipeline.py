4db1097eca294a6525231364af02b4d029b5edcf
WhiteP42 <pau.rodriguez.riera@gmail.com>
2025-12-27 18:03:01 +0100

    added model and error for not supported model.

--- src/bioMEMEPy/pipeline.py
+++ src/bioMEMEPy/pipeline.py
@@ -1,6 +1,8 @@
-def meme(fasta, alphabet, motif_l, loops):
+def meme(fasta, alphabet, motif_l, loops, model):
     # Errors
     if not isinstance(motif_l, int):
         raise TypeError('Length of the motif must be int.')
-    if not isinstance(loops, (int or bool)):
+    if not isinstance(loops, int):
         raise TypeError('Amount of loops must be either int or None.')
+    if model not in ['oops', 'zoops', 'anr']:
+        raise ModuleNotFoundError('Mode not supported.')
