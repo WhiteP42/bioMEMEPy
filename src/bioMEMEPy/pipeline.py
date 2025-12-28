6650ee5bc40a2bf6c280617e2278ebbfa35eb452
WhiteP42 <pau.rodriguez.riera@gmail.com>
2025-12-28 19:23:54 +0100

    Deleted biopython dependency

--- src/bioMEMEPy/pipeline.py
+++ src/bioMEMEPy/pipeline.py
@@ -1,6 +1,6 @@
 import logging
-from Bio import SeqIO
 from .oops import oops
+from .tools import extract
 
 def meme(fasta, alphabet, motif_l, model):
     # Errors
@@ -9,7 +9,7 @@ def meme(fasta, alphabet, motif_l, model):
 
     #Read FASTA
     with open(fasta, 'r') as f:
-        seqs = list(SeqIO.parse(f, 'fasta'))
+        seqs = extract(fasta)
 
     #Call mode
     _models = {'oops': oops} # Future models to be implemented here.
